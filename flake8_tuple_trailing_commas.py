dc2d499278937d86246abe705991ed208439e914
Drozdov David <36568373+davemus@users.noreply.github.com>
2021-03-31 16:25:57 +0300

    Add trailing commas in tuple checker

---
+++ flake8_tuple_trailing_commas.py
@@ -0,0 +1,45 @@
+import ast
+import sys
+
+if sys.version_info < (3, 8):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
+
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self):
+        self.tuple_closing_bracket_positions = []
+
+    def visit_Tuple(self, node):
+        self.tuple_closing_bracket_positions.append((node.end_lineno, node.end_col_offset))
+        self.generic_visit(node)
+
+
+_MSG_MISSING_COMMA_TUPLE = 'FNA100 trailing comma in tuple is missing'
+
+
+class Plugin:
+    name = __name__
+    version = importlib_metadata.version(__name__)
+
+    def __init__(self, tree, file_tokens):
+        self._ast = tree
+        self._file_tokens = file_tokens
+
+    def validate_previous_tokens(self, token_idx):
+        if token_idx == 0:
+            return True
+        token_idx -= 1
+        token = self._file_tokens[token_idx]
+        if token.type == 4:
+            return self.validate_previous_tokens(token_idx)
+        return token.type == 54 and token.string == ','
+
+    def run(self):
+        visitor = Visitor()
+        visitor.visit(self._ast)
+        for position in visitor.tuple_closing_bracket_positions:
+            idx = self._file_tokens.index(next(x for x in self._file_tokens if x.end == position))
+            if not self.validate_previous_tokens(idx):
+                yield position[0], position[1], _MSG_MISSING_COMMA_TUPLE, type(self)
---
+++ flake8_tuple_trailing_commas_test.py
@@ -0,0 +1,31 @@
+import ast
+import tokenize
+import unittest
+from io import BytesIO
+
+from flake8_tuple_trailing_commas import Plugin
+
+
+def _results(code):
+    ast_tree = ast.parse(code)
+    tokens = list(tokenize.tokenize(BytesIO(bytes(code, encoding='utf-8')).readline))
+    plugin = Plugin(ast_tree, tokens)
+    return {f'{line},{offset}: {msg}' for line, offset, msg, _ in plugin.run()}
+
+
+class TestPlugin(unittest.TestCase):
+    def test_trivial_case(self):
+        self.assertSetEqual(set(), _results(''))
+
+    def test_with_comma_tuple(self):
+        self.assertSetEqual(set(), _results('(1, 2, 3,)'))
+
+    def test_without_comma_tuple(self):
+        self.assertSetEqual(
+            set(('1,9: FNA100 trailing comma in tuple is missing',)),
+            _results('(1, 2, 3)')
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
